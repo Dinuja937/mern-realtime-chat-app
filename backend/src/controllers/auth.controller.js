96946b6abb94ee92a9cce7ded7a57f3a0f1f6d9c
Dinuja937 <agrmdcranaweera@gmail.com>
2026-05-26 00:51:57 +0530

    feat: Implement auth controller with signup/login/logout and middleware

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -52,10 +52,46 @@ export const signup = async (req,res) => {
     }    
 };
 
-export const login = (req,res) => {
-    res.send("login router");
+export const login = async (req,res) => {
+    const{email,password} = req.body;
+
+    try{
+        const user = await User.findOne({email});
+
+        if(!user){
+            return res.status(400).json({message: "Invalid Credentials"})
+        }
+
+        const isPasswordCorrect = await bcrypt.compare(password, user.password);
+        if(!isPasswordCorrect){
+            return res.status(400).json({message: "Invalid Credentials"})
+        }
+
+        generateToken(user._id,res);
+
+        res.status(200).json({
+            _id:user._id,
+            fullName:user.username,
+            email:user.email,
+            profilePic:user.profilePic,
+        })
+
+    }catch(error){
+        console.log("Error in login controller", error.message);
+
+    }
 };
 
-export const logout = (req,res) => {
-    res.send("logout router");
+export const logout = async (req,res) => {
+
+    try{
+        res.cookie("jwt", "", {maxAge:0});
+        res.status(200).json({message: "Logged out successfully"});
+
+    }catch(error){
+        console.log("Error in logout controller", error.message);
+        res.status(500).json({message: "Internal Server Error"});
+    }
 };
+
+export const updateProfile = async (req, res) => {};
