259683cb51907deda1c387f70157c896d3e57759
Dinuja937 <agrmdcranaweera@gmail.com>
2026-06-13 23:02:54 +0530

    feat: add routing and new pages for Home, Login, Sign Up, Settings, and Profile

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import HomePage from './pages/HomePage'
+import LoginPage from './pages/LoginPage'
+import SignUpPage from './pages/SignUpPage'
+import SettingsPage from './pages/SettingsPage'
+import ProfilePage from './pages/ProfilePage'
 
 const App = () => {
   return (
     <div>
       <Navbar />
+
+      <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/settings" element={<SettingsPage />} />
+          <Route path="/profile" element={<ProfilePage />} />
+      </Routes>
+
     </div>
   )
 }
