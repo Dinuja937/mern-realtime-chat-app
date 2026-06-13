c5520531888f795403bc80845e0f0da327438bbc
Dinuja937 <agrmdcranaweera@gmail.com>
2026-06-14 01:24:37 +0530

    feat: add lucide-react for loading icon and enhance authentication check in App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,14 +6,28 @@ import LoginPage from './pages/LoginPage'
 import SignUpPage from './pages/SignUpPage'
 import SettingsPage from './pages/SettingsPage'
 import ProfilePage from './pages/ProfilePage'
+import { LoaderIcon } from 'react-hot-toast'
+
+import { useEffect } from 'react'
+import { useAuthStore } from './store/useAuthStore'
+
+import { Loader } from 'lucide-react';
 
 const App = () => {
 
-  const {authUser,checkAuth} = userAuthStore();  
+  const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
 
   useEffect(() => {
     checkAuth();
   },[checkAuth]);
+
+  console.log({ authUser });
+
+  if(isCheckingAuth && !authUser) return (
+    <div className='flex items-center justify-center h-screen'>
+      <Loader className="size-10 animate-spin" />
+    </div>
+  )
 
   return (
     <div>
