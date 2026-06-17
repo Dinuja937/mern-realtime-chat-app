e524d71927e20b6687fcfe79d279bf98f6249d1c
Dinuja937 <agrmdcranaweera@gmail.com>
2026-06-17 16:34:46 +0530

    feat: implement signup state action and configure toast alerts with route guards

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -1,5 +1,5 @@
-import {create} from 'zustand';
-import {axiosInstance} from '../lib/axios.js';
+import { create } from 'zustand';
+import { axiosInstance } from '../lib/axios.js';
 
 export const useAuthStore = create((set) => ({
     authUser: null,
@@ -13,13 +13,18 @@ export const useAuthStore = create((set) => ({
         try {
             const res = await axiosInstance.get('/auth/check');
 
-            set({authUser:res.data})
+            set({ authUser: res.data })
 
-        }catch (error) {
+        } catch (error) {
             console.log("Error in checkAuth:", error);
-            set({authUser:null});
-        }finally {
-            set({isCheckingAuth:false});
+            set({ authUser: null });
+        } finally {
+            set({ isCheckingAuth: false });
         }
     },
+
+    signup: async (data) => {
+
+
+    }
 }));
